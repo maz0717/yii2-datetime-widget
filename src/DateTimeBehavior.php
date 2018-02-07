81bd2ab3a4ca10c1f21d114ed55ddf2060fc0595
John Maslanik <maz@mlx.net>
2018-02-07 20:37:30 +0800

    ignore expressions when converting date

--- src/DateTimeBehavior.php
+++ src/DateTimeBehavior.php
@@ -6,6 +6,7 @@ use Yii;
 use yii\base\Behavior;
 use yii\db\BaseActiveRecord;
 use yii\base\ModelEvent;
+use Expression;
 
 
 class DateTimeBehavior extends Behavior {
@@ -50,18 +51,31 @@ class DateTimeBehavior extends Behavior {
 
         foreach($this->dateTimeAttributes as $dateField) {
             $field = $this->owner->$dateField;
-            // Skip if already the save format
-            if($field && !preg_match($this->saveFormatRegex,$field)) {
-                $date = \DateTime::createFromFormat($this->datetimeFormat,$field);
-                if(!$date) {
-                    // TODO
-                    echo $field . PHP_EOL;
-                    echo var_dump(\DateTime::getLastErrors());
-                    exit;
-                }
-                $date->setTimezone(new \DateTimeZone($this->saveTimezone));
-                $this->owner->$dateField = $date->format($this->saveFormat);
+
+            // Skip if blank
+            if(!$field) {
+                continue;
             }
+
+            // Skip expressions;
+            if ($field instanceof Expression || strpos($field, '(') !== false) {
+                continue;
+            }
+
+            // Skip if already the save format
+            if(preg_match($this->saveFormatRegex,$field)) {
+                continue;
+            }
+
+            $date = \DateTime::createFromFormat($this->datetimeFormat,$field);
+            if(!$date) {
+                // TODO
+                echo $field . PHP_EOL;
+                echo var_dump(\DateTime::getLastErrors());
+                exit;
+            }
+            $date->setTimezone(new \DateTimeZone($this->saveTimezone));
+            $this->owner->$dateField = $date->format($this->saveFormat);
         }
     }
 
